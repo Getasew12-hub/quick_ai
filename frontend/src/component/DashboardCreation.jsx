d3c532fdc302ed51683d1359bcd6ea6cde10a25f
Getasew Tewachew <getasewtewachew@gmail.com>
2026-02-18 07:55:50 -0800

    image display fix

--- frontend/src/component/DashboardCreation.jsx
+++ frontend/src/component/DashboardCreation.jsx
@@ -23,7 +23,7 @@ function DashboardCreation({item}) {
     
 
   <div className='hellow rever-rw overflow-x-hidden!' >
-        {moreData && (item?.type=="image" ? <img src={item?.content} alt="image" className='max-h-96! max-sm:h-60! object-contain! max-w-full '/> :<Markdown>{item?.content}</Markdown>) }
+        {moreData && (item?.type=="image" ? <img src={item?.content} alt="image" className='max-h-96! max-sm:h-60! object-cover! max-w-full! '/> :<Markdown>{item?.content}</Markdown>) }
       </div>
 </div>
   )
